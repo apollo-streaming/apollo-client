30fdd0bda04cb185da87a65f214e52e039c1f184
thallesnct <thallescarvalho12@gmail.com>
2020-05-09 23:51:01 -0300

    Adicoionado import da pagina de resultados

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
 
 function Routes() {
   return (
