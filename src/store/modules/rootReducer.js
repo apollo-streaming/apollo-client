3ec4bbccfb4919ddb14f8a7ba09b5ce2cc6903ea
thallesnct <thallescarvalho12@gmail.com>
2020-05-16 12:07:59 -0300

    Adicionados estados de episodios e podcasts

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
 import search from './search/reducer';
+import episodes from './episodes/reducer';
+import podcasts from './podcasts/reducer';
 
-export default combineReducers({ search });
+export default combineReducers({ search, episodes, podcasts });
